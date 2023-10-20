a441f844af5b97966a6fa9336e9a1d625b084560
Anant <anantpatel07012003@gmail.com>
2023-10-20 23:07:11 +0530

    code and logic improved

--- src/components/RecommendationBox.tsx
+++ src/components/RecommendationBox.tsx
@@ -1,93 +1,46 @@
-import { faker } from "@faker-js/faker";
 import React from "react";
+import { faker } from "@faker-js/faker";
 import styles from "./recommendationBox.module.css";
 
 interface RecommendationBoxProps {}
 
+const generateRandomProduct = () => {
+  return {
+    productName: faker.commerce.productName(),
+    imageUrl: faker.image.sports(90, 122, true),
+  };
+};
+
 export const RecommendationBox: React.FC<RecommendationBoxProps> = () => {
+  const trends = Array(5)
+    .fill(null)
+    .map(() => generateRandomProduct());
+
+  const suggestions = Array(5)
+    .fill(null)
+    .map(() => generateRandomProduct());
+
   return (
     <div className={styles.RecommendationBoxWrapper}>
       <div className={styles.RecommendationBox}>
         <div className={styles.Header}>Latest Trends</div>
         <div className={styles.Trends}>
-          <div className={styles.Trend}>
-            <img
-              className={styles.TrendImg}
-              src={faker.image.sports(90, 122, true)}
-              alt=""
-            />
-            <div className={styles.TrendName}>
-              {faker.commerce.productName()}
+          {trends.map((trend, index) => (
+            <div className={styles.Trend} key={index}>
+              <img className={styles.TrendImg} src={trend.imageUrl} alt="" />
+              <div className={styles.TrendName}>{trend.productName}</div>
             </div>
-          </div>
-          <div className="Trend">
-            <img
-              className={styles.TrendImg}
-              src={faker.image.sports(90, 122, true)}
-              alt=""
-            />
-            <div className={styles.TrendName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className="Trend">
-            <img
-              className={styles.TrendImg}
-              src={faker.image.sports(90, 122, true)}
-              alt=""
-            />
-            <div className={styles.TrendName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className="Trend">
-            <img
-              className={styles.TrendImg}
-              src={faker.image.sports(90, 122, true)}
-              alt=""
-            />
-            <div className={styles.TrendName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className="Trend">
-            <img
-              className={styles.TrendImg}
-              src={faker.image.sports(90, 122, true)}
-              alt=""
-            />
-            <div className={styles.TrendName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
+          ))}
         </div>
         <div className={styles.Header}>Popular Suggestions</div>
         <div className={styles.Suggestions}>
-          <div className={styles.Suggestion}>
-            <div className={styles.SuggestionName}>
-              {faker.commerce.productName()}
+          {suggestions.map((suggestion, index) => (
+            <div className={styles.Suggestion} key={index}>
+              <div className={styles.SuggestionName}>
+                {suggestion.productName}
+              </div>
             </div>
-          </div>
-          <div className={styles.Suggestion}>
-            <div className={styles.SuggestionName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className={styles.Suggestion}>
-            <div className={styles.SuggestionName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className={styles.Suggestion}>
-            <div className={styles.SuggestionName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
-          <div className={styles.Suggestion}>
-            <div className={styles.SuggestionName}>
-              {faker.commerce.productName()}
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,3 +1,203 @@
+// import React, { useEffect, useState } from "react";
+// import styles from "./searchResults.module.css";
+// import { SearchBar } from "../components/SearchBar";
+// import { GetRatingsUI } from "../components/GetRatingsUI";
+// import { Product } from "../Models/Product";
+// import { GetProducts } from "../DataGenerator/GetProducts";
+// import { ProductView } from "../components/ProductView";
+
+// interface SearchResultsProps {}
+
+// export const SearchResults: React.FC<SearchResultsProps> = () => {
+//   const [products, setProducts] = useState<Product[]>();
+//   const [brand, setBrand] = useState<string>("All");
+//   const [maxPrice, setMaxPrice] = useState<string>("1000");
+//   const [rating, setRating] = useState<string>("5");
+
+//   useEffect(() => {
+//     let minPrice = maxPrice === "1000" ? 500 : 1;
+//     setProducts(
+//       GetProducts(brand, minPrice, parseFloat(maxPrice), parseInt(rating))
+//     );
+//   }, [brand, maxPrice, rating]);
+
+//   const handleBrandChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setBrand(e.target.value);
+//   };
+
+//   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setMaxPrice(e.target.value);
+//   };
+
+//   const handleRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setRating(e.target.value);
+//   };
+
+//   return (
+//     <div className={styles.SearchResults}>
+//       <div className={styles.SearchBar}>
+//         <SearchBar />
+//       </div>
+//       <br />
+//       <br />
+//       <div className={styles.SearchResultsHeader}>Search Results</div>
+//       <div className={styles.Container}>
+//         <div className={styles.Filters}>
+//           <div className={styles.Filter}>
+//             <div className={styles.FilterHeader}>
+//               <span className={styles.FilterName}>BRAND</span>
+//               <img
+//                 src="./expandIcon.png"
+//                 alt="Expand"
+//                 className={styles.ExpandIcon}
+//               />
+//             </div>
+//             <div className={styles.FilterOptions}>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleBrandChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   id="Mango"
+//                   type="radio"
+//                   name="brand"
+//                   value="Boots"
+//                 />
+//                 <label htmlFor="Boots">Boot</label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleBrandChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   id="H&M"
+//                   type="radio"
+//                   name="brand"
+//                   value="Football"
+//                 />
+//                 <label htmlFor="Football">Football</label>
+//               </div>
+//             </div>
+//           </div>
+//           <div className={styles.Filter}>
+//             <div className={styles.FilterHeader}>
+//               <span className={styles.FilterName}>PRICE RANGE</span>
+//               <img
+//                 src="./expandIcon.png"
+//                 alt="Expand"
+//                 className={styles.ExpandIcon}
+//               />
+//             </div>
+//             <div className={styles.FilterOptions}>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handlePriceChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   id="price500"
+//                   type="radio"
+//                   name="price"
+//                   value="500"
+//                 />
+//                 <label htmlFor="price500">Under ₹500</label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handlePriceChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   id="price1000"
+//                   type="radio"
+//                   name="price"
+//                   value="1000"
+//                 />
+//                 <label htmlFor="price1000">₹500 to ₹1000</label>
+//               </div>
+//             </div>
+//           </div>
+//           <div className={styles.Filter}>
+//             <div className={styles.FilterHeader}>
+//               <span className={styles.FilterName}>RATINGS</span>
+//               <img
+//                 src="./expandIcon.png"
+//                 alt="Expand"
+//                 className={styles.ExpandIcon}
+//               />
+//             </div>
+//             <div className={styles.FilterOptions}>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleRatingChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   type="radio"
+//                   name="rating"
+//                   id="5*"
+//                   value="5"
+//                 />
+//                 <label htmlFor="5*">
+//                   <GetRatingsUI Rating={5} />
+//                 </label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleRatingChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   type="radio"
+//                   name="rating"
+//                   id="4*"
+//                   value="4"
+//                 />
+//                 <label htmlFor="4*">
+//                   <GetRatingsUI Rating={4} />
+//                 </label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleRatingChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   type="radio"
+//                   name="rating"
+//                   id="3*"
+//                   value="3"
+//                 />
+//                 <label htmlFor="3*">
+//                   <GetRatingsUI Rating={3} />
+//                 </label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleRatingChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   type="radio"
+//                   name="rating"
+//                   id="2*"
+//                   value="2"
+//                 />
+//                 <label htmlFor="2*">
+//                   <GetRatingsUI Rating={2} />
+//                 </label>
+//               </div>
+//               <div className={styles.FilterOption}>
+//                 <input
+//                   onChange={(e) => handleRatingChange(e)}
+//                   className={styles.FilterCheckbox}
+//                   type="radio"
+//                   name="rating"
+//                   id="1*"
+//                   value="1"
+//                 />
+//                 <label htmlFor="1*">
+//                   <GetRatingsUI Rating={1} />
+//                 </label>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className={styles.Products}>
+//           {products?.map((p) => (
+//             <ProductView p={p} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 import React, { useEffect, useState } from "react";
 import styles from "./searchResults.module.css";
 import { SearchBar } from "../components/SearchBar";
@@ -9,28 +209,27 @@ import { ProductView } from "../components/ProductView";
 interface SearchResultsProps {}
 
 export const SearchResults: React.FC<SearchResultsProps> = () => {
+  const [filters, setFilters] = useState({
+    brand: "All",
+    maxPrice: "1000",
+    rating: "5",
+  });
   const [products, setProducts] = useState<Product[]>();
-  const [brand, setBrand] = useState<string>("All");
-  const [maxPrice, setMaxPrice] = useState<string>("1000");
-  const [rating, setRating] = useState<string>("5");
 
   useEffect(() => {
-    let minPrice = maxPrice === "1000" ? 500 : 1;
+    let minPrice = filters.maxPrice === "1000" ? 500 : 1;
     setProducts(
-      GetProducts(brand, minPrice, parseFloat(maxPrice), parseInt(rating))
+      GetProducts(
+        filters.brand,
+        minPrice,
+        parseFloat(filters.maxPrice),
+        parseInt(filters.rating)
+      )
     );
-  }, [brand, maxPrice, rating]);
+  }, [filters]);
 
-  const handleBrandChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBrand(e.target.value);
-  };
-
-  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMaxPrice(e.target.value);
-  };
-
-  const handleRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRating(e.target.value);
+  const handleFilterChange = (filterName: string, value: string) => {
+    setFilters({ ...filters, [filterName]: value });
   };
 
   return (
@@ -43,6 +242,8 @@ export const SearchResults: React.FC<SearchResultsProps> = () => {
       <div className={styles.SearchResultsHeader}>Search Results</div>
       <div className={styles.Container}>
         <div className={styles.Filters}>
+          {/* Brand Filter */}
+          {/* Replace "All" with your brand options */}
           <div className={styles.Filter}>
             <div className={styles.FilterHeader}>
               <span className={styles.FilterName}>BRAND</span>
@@ -53,30 +254,23 @@ export const SearchResults: React.FC<SearchResultsProps> = () => {
               />
             </div>
             <div className={styles.FilterOptions}>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleBrandChange(e)}
-                  className={styles.FilterCheckbox}
-                  id="Mango"
-                  type="radio"
-                  name="brand"
-                  value="Boots"
-                />
-                <label htmlFor="Boots">Boot</label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleBrandChange(e)}
-                  className={styles.FilterCheckbox}
-                  id="H&M"
-                  type="radio"
-                  name="brand"
-                  value="Football"
-                />
-                <label htmlFor="Football">Football</label>
-              </div>
+              {["All", "Boots", "Football"].map((brandOption) => (
+                <div className={styles.FilterOption} key={brandOption}>
+                  <input
+                    onChange={(e) => handleFilterChange("brand", brandOption)}
+                    className={styles.FilterCheckbox}
+                    type="radio"
+                    name="brand"
+                    value={brandOption}
+                    checked={filters.brand === brandOption}
+                  />
+                  <label htmlFor={brandOption}>{brandOption}</label>
+                </div>
+              ))}
             </div>
           </div>
+
+          {/* Price Range Filter */}
           <div className={styles.Filter}>
             <div className={styles.FilterHeader}>
               <span className={styles.FilterName}>PRICE RANGE</span>
@@ -87,30 +281,27 @@ export const SearchResults: React.FC<SearchResultsProps> = () => {
               />
             </div>
             <div className={styles.FilterOptions}>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handlePriceChange(e)}
-                  className={styles.FilterCheckbox}
-                  id="price500"
-                  type="radio"
-                  name="price"
-                  value="500"
-                />
-                <label htmlFor="price500">Under ₹500</label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handlePriceChange(e)}
-                  className={styles.FilterCheckbox}
-                  id="price1000"
-                  type="radio"
-                  name="price"
-                  value="1000"
-                />
-                <label htmlFor="price1000">₹500 to ₹1000</label>
-              </div>
+              {["500", "1000"].map((priceOption) => (
+                <div className={styles.FilterOption} key={priceOption}>
+                  <input
+                    onChange={(e) =>
+                      handleFilterChange("maxPrice", priceOption)
+                    }
+                    className={styles.FilterCheckbox}
+                    type="radio"
+                    name="price"
+                    value={priceOption}
+                    checked={filters.maxPrice === priceOption}
+                  />
+                  <label htmlFor={`price${priceOption}`}>
+                    Under ₹{priceOption}
+                  </label>
+                </div>
+              ))}
             </div>
           </div>
+
+          {/* Ratings Filter */}
           <div className={styles.Filter}>
             <div className={styles.FilterHeader}>
               <span className={styles.FilterName}>RATINGS</span>
@@ -121,71 +312,23 @@ export const SearchResults: React.FC<SearchResultsProps> = () => {
               />
             </div>
             <div className={styles.FilterOptions}>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleRatingChange(e)}
-                  className={styles.FilterCheckbox}
-                  type="radio"
-                  name="rating"
-                  id="5*"
-                  value="5"
-                />
-                <label htmlFor="5*">
-                  <GetRatingsUI Rating={5} />
-                </label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleRatingChange(e)}
-                  className={styles.FilterCheckbox}
-                  type="radio"
-                  name="rating"
-                  id="4*"
-                  value="4"
-                />
-                <label htmlFor="4*">
-                  <GetRatingsUI Rating={4} />
-                </label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleRatingChange(e)}
-                  className={styles.FilterCheckbox}
-                  type="radio"
-                  name="rating"
-                  id="3*"
-                  value="3"
-                />
-                <label htmlFor="3*">
-                  <GetRatingsUI Rating={3} />
-                </label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleRatingChange(e)}
-                  className={styles.FilterCheckbox}
-                  type="radio"
-                  name="rating"
-                  id="2*"
-                  value="2"
-                />
-                <label htmlFor="2*">
-                  <GetRatingsUI Rating={2} />
-                </label>
-              </div>
-              <div className={styles.FilterOption}>
-                <input
-                  onChange={(e) => handleRatingChange(e)}
-                  className={styles.FilterCheckbox}
-                  type="radio"
-                  name="rating"
-                  id="1*"
-                  value="1"
-                />
-                <label htmlFor="1*">
-                  <GetRatingsUI Rating={1} />
-                </label>
-              </div>
+              {[5, 4, 3, 2, 1].map((ratingOption) => (
+                <div className={styles.FilterOption} key={ratingOption}>
+                  <input
+                    onChange={(e) =>
+                      handleFilterChange("rating", String(ratingOption))
+                    }
+                    className={styles.FilterCheckbox}
+                    type="radio"
+                    name="rating"
+                    value={String(ratingOption)}
+                    checked={filters.rating === String(ratingOption)}
+                  />
+                  <label htmlFor={`${ratingOption}*`}>
+                    <GetRatingsUI Rating={ratingOption} />
+                  </label>
+                </div>
+              ))}
             </div>
           </div>
         </div>
